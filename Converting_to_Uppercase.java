494f930d833c4eb45cbd9c440b94ef3d97b5f0bd
Kanishka2703 <kanishkaasinghhh27@gmail.com>
2022-10-18 19:44:26 +0530

    Converting string to Uppercase without using inbuilt function

---
+++ Converting_to_Uppercase.java
@@ -0,0 +1,21 @@
+public class Main 
+{
+    public static void main(String args[])
+    {
+        String input = "java is best";
+        System.out.println("String is : " + input);
+        char strArr[] = input.toCharArray();
+        for (int i = 0; i < strArr.length; i++)
+        {
+            if (strArr[i] >= 'a' && strArr[i] <= 'z') 
+            {
+                strArr[i] = (char) ((int) strArr[i] - 32);
+            }
+        }
+                System.out.print("Upper case string is : ");
+                for (int i = 0; i < strArr.length; i++)
+                {
+                    System.out.print(strArr[i]);
+                }
+    }
+}
